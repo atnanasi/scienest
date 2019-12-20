71def4be3d74b27356f05a6b3d8a1f0c2d2c95d2
Rokoucha <rokoucha@rokoucha.net>
2019-12-20 15:25:20 +0900

    getEntry api

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,16 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts'
 import entry from './handlers/entry.tsx'
 import getEntries from './handlers/getEntries.ts'
+import getEntry from './handlers/getEntry.ts'
 import index from './handlers/index.tsx'
 
-const router = new Router()
+const router = new Router({ strict: true })
 
 router.get('/', index)
-router.get('/entries', entry)
+router.get('/entries/', entry)
 router.get('/entries/:path', entry)
 router.get('/api/entries', getEntries)
+router.get('/api/entries/', getEntry)
+router.get('/api/entries/:path', getEntry)
 
 export default router
