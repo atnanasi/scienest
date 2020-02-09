a6c663e06298bf4676b9d044439106dae70a00da
Rokoucha <rokoucha@rokoucha.net>
2020-02-09 20:45:50 +0900

    refactor: Remove router file

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { APP_HOST, APP_PORT, APP_ENV } from './config.ts'
 import { Application } from 'https://deno.land/x/oak/mod.ts'
-import api from './routers/api.ts'
+import { Router } from 'https://deno.land/x/oak/mod.ts'
+import * as entry from './handlers/entries.ts'
 import client from './database/postgres/client.ts'
+import entryView from './views/entry.tsx'
+import index from './handlers/index.ts'
+import indexView from './views/index.tsx'
 import log from './utils/logger.ts'
 import requestLogger from './middleware/requestLogger.ts'
 import responseTime from './middleware/responseTime.ts'
-import view from './routers/view.ts'
 
 async function main() {
   if (APP_ENV === 'development') {
@@ -15,14 +18,25 @@ async function main() {
   log.info('Connecting to database')
   await client.connect()
 
+  const router = new Router({ strict: true })
+
+  router.get('/api', index)
+  router.get('/api/entries', entry.getEntries)
+  router.post('/api/entries', entry.createEntry)
+  router.get('/api/entries/', entry.getEntry)
+  router.get('/api/entries/:path', entry.getEntry)
+  router.put('/api/entries/:path', entry.updateEntry)
+  router.delete('/api/entries/:path', entry.deleteEntry)
+  router.get('/', indexView)
+  router.get('/entries/', entryView)
+  router.get('/entries/:path', entryView)
+  
   const app = new Application()
 
   app.use(requestLogger)
   app.use(responseTime)
-  app.use(api.routes())
-  app.use(api.allowedMethods())
-  app.use(view.routes())
-  app.use(view.allowedMethods())
+  app.use(router.routes())
+  app.use(router.allowedMethods())
 
   log.info(`Listenig on ${APP_HOST}:${APP_PORT}`)
   await app.listen(`${APP_HOST}:${APP_PORT}`)
